f6338f6273f271ff05d92793c45dbe969da17dc1
Juliya <57349170+Juliya-Novoselskaya0@users.noreply.github.com>
2020-12-23 12:05:09 +0300

    Update LR7_NEW.cpp

--- LR7_NEW.cpp
+++ LR7_NEW.cpp
@@ -1,4 +1,4 @@
-﻿// LR7_NEW.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// LR7_NEW.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
@@ -10,26 +10,25 @@ template <class T>
 class dateElem
 {
 public:
-    T number;
+	T number;
 };
 
 template <class T>
 class date
 {
-    dateElem<T>* day,* month,* year;
+	dateElem<T>* day, * month, * year;
 public:
-    date():day(0), month(0), year(0) {};
-    date(dateElem<T>* d, dateElem<T>* m, dateElem<T>* y) {
-        day = d;
-        month = m;
-        year = y;
-    }
-  //  ~date();
-    date<T>* changeDate(date<T>* ob);  //worked
-    int numberBeforeNY(date<T>* ob);   //worked
-    bool operator !=(date<T>* ob); //worked
-    date<T>* operator-(date<T>* ob);
-    void Show_date(date<T>* obj); //worked
+	date() :day(0), month(0), year(0) {};
+	date(dateElem<T>* d, dateElem<T>* m, dateElem<T>* y) {
+		day = d;
+		month = m;
+		year = y;
+	}
+	date<T>* changeDate(date<T>* ob); 
+	int numberBeforeNY(date<T>* ob);   
+	bool operator !=(date<T>* ob); 
+	date<T>* operator-(date<T>* ob);
+	void Show_date(date<T>* obj);
 };
 
 template <class T>
@@ -145,7 +144,6 @@ date<T>* date<T>::operator-(date<T>* ob)
 	m1->number = m;
 	dateElem<int>* y1 = new dateElem<int>;
 	y1->number = y;
-	//date<T>* objRez = new date<T>(d1, m1, y1);
 	differenceRez = new date<int>(d1, m1, y1);
 	return differenceRez;
 }
@@ -153,109 +151,79 @@ date<T>* date<T>::operator-(date<T>* ob)
 template <class T>
 bool date<T>::operator !=(date<T>* da)
 {
-    if (date::day->number == da->day->number && date::month->number == da->month->number && date::year->number == da->year->number)
-        return false;
-    else
-    {
-        return true;
-    }
+	if (date::day->number == da->day->number && date::month->number == da->month->number && date::year->number == da->year->number)
+		return false;
+	else
+	{
+		return true;
+	}
 }
 
 template <class T>
 int date<T>::numberBeforeNY(date<T>* ob)
 {
-    int number = 0;
-    int daysMonth[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
-    if (ob->month->number == 12)
-    {
-        number = 31 - ob->day->number;
-    }
-    else {
-        number = daysMonth[(ob->month->number - 1)] - ob->day->number;
-        int i = ob->month->number;
-        while (i != 12)
-        {
-            number += daysMonth[(i - 1)];
-            i++;
-        }
-    }
-    return number;
+	int number = 0;
+	int daysMonth[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	if (ob->month->number == 12)
+	{
+		number = 31 - ob->day->number;
+	}
+	else {
+		number = daysMonth[(ob->month->number - 1)] - ob->day->number;
+		int i = ob->month->number;
+		while (i != 12)
+		{
+			number += daysMonth[(i - 1)];
+			i++;
+		}
+	}
+	return number;
 }
 
 template <class T>
 void date<T>::Show_date(date<T>* obj)
 {
-    if (obj == NULL) {
-        return;
-    }
-    cout << obj->day->number;
-    cout << ".";
-    cout << obj->month->number;
-    cout << ".";
-    cout << obj->year->number << endl;
+	if (obj == NULL) {
+		return;
+	}
+	cout << obj->day->number;
+	cout << ".";
+	cout << obj->month->number;
+	cout << ".";
+	cout << obj->year->number << endl;
 }
 
 template <class T>
 date<T>* date<T>::changeDate(date<T>* ob)
 {
-    int d, m, y;
-    cout << "Day=";
-    cin >> d;
-    cout << endl;
-    cout << "Month=";
-    cin >> m;
-    cout << endl;
-    cout << "Year=";
-    cin >> y;
-    cout << endl;
-    dateElem<int>* d1 = new dateElem<int>;
-    d1->number = d;
-    dateElem<int>* m1 = new dateElem<int>;
-    m1->number = m;
-    dateElem<int>* y1 = new dateElem<int>;
-    y1->number = y;
-    date<T>* objRez = new date<T>(d1,m1,y1);
-    ob->day = d1;
-    ob->month= m1;
-    ob->year=y1;
-    return objRez;
+	int d, m, y;
+	cout << "Day=";
+	cin >> d;
+	cout << endl;
+	cout << "Month=";
+	cin >> m;
+	cout << endl;
+	cout << "Year=";
+	cin >> y;
+	cout << endl;
+	dateElem<int>* d1 = new dateElem<int>;
+	d1->number = d;
+	dateElem<int>* m1 = new dateElem<int>;
+	m1->number = m;
+	dateElem<int>* y1 = new dateElem<int>;
+	y1->number = y;
+	date<T>* objRez = new date<T>(d1, m1, y1);
+	ob->day = d1;
+	ob->month = m1;
+	ob->year = y1;
+	return objRez;
 }
 
 int main()
 {
-    dateElem<int>* a = new dateElem<int>;
-    date<int> daaaat;
-    date<int>* example = new date<int>(a,a,a);
-   // daaaat.Show_date(example);
-   // example=daaaat.changeDate(example);
-   /* daaaat.changeDate(example);
-   int m= daaaat.numberBeforeNY(example);
-   cout << "Number before Ney Year=" << m << endl;
-    daaaat.Show_date(example);
-
-    cout << "Date to test for equality";
-    cout << endl;
-    date<int>* forCheck = new date<int>(a,a,a);
-    daaaat.changeDate(forCheck);
-   // if(example!=forCheck)
-    if (example->operator!=(forCheck))
-    {
-        cout << "Not equal! =(";
-    }
-    else {
-        cout << "Are equal! =)";
-    }
-    cout << endl;
-
-	cout << "Date to search for a gap";
-	cout << endl;
-	date<int>* forCheck2 = new date<int>(a,a,a);
-	daaaat.changeDate(forCheck2);
-	date<int>* rez2 = example->operator-(forCheck2);
-	cout << "Difference between dates=";
-	daaaat.Show_date(rez2);
-	*/
-
+	dateElem<int>* a = new dateElem<int>;
+	date<int> daaaat;
+	date<int>* example = new date<int>(a, a, a);
 	int ch;
 	do {
 		cout << endl << endl << "1 - change date; \n2 - determine the number of days before the new year; \n3 - output on display; \n4 - inequality test; \n5 - interval between two dates; \n6 - end" << endl;
@@ -263,11 +231,14 @@ int main()
 		cin >> ch;
 		switch (ch) {
 		case 1:
-			cout << "Сhange of date";
+			rewind;
+			cout << "Change of date";
+			cout << endl;
 			daaaat.changeDate(example);
 			break;
 		case 2:
-			cout << "Сalculating the number of days" << endl;
+			rewind;
+			cout << "Calculating the number of days" << endl;
 			cout << "Number before New Year=" << daaaat.numberBeforeNY(example);
 			break;
 		case 3:
@@ -278,7 +249,6 @@ int main()
 		cout << endl;
 		date<int>* forCheck = new date<int>(a, a, a);
 		daaaat.changeDate(forCheck);
-		// if(example!=forCheck)
 		if (example->operator!=(forCheck))
 		{
 			cout << "Not equal! =(";
